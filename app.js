43bd088e065c10a83c5fccba03f83188b2bcb21f
Lomatul Mahzabin <90206489+lomatul@users.noreply.github.com>
2023-03-24 19:10:04 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+var x= document.getElementById("login");
+var y= document.getElementById("registration");
+var z= document.getElementById("btn");
+
+
+function registration(){
+    x.style.left="-450px";
+    y.style.left="50px";
+    z.style.left="110px";
+}
+
+function login(){
+    x.style.left="50px";
+    y.style.left="450px";
+    z.style.left="0px";
+}
